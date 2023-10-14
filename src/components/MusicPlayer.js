270cd196a837b96eb69dbbea051f2d3652144247
Juan Pablo Juliao <jpjuliao@gmail.com>
2023-10-14 01:40:50 -0500

    Update MusicPlayer.js

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,32 +1,64 @@
-mport React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function MusicPlayer() {
-	  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null;
 
-	  const handlePlayPause = () => {
-		      setIsPlaying(!isPlaying);
-		    };
+  const songs = [
+    {
+      title: 'Song 1',
+      source: '/music/song1.mp3',
+    },
+    {
+      title: 'Song 2',
+      source: '/music/song2.mp3',
+    }
+  ];
 
-	  const handleNextTrack = () => {
-		      // Implement logic to switch to the next track
-		  //   };
-		  //
-		  //     const handlePrevTrack = () => {
-		  //         // Implement logic to switch to the previous track
-		  //           };
-		  //
-		  //             return (
-		  //                 <div>
-		  //                       <h2>Now Playing: Song Title</h2>
-		  //                             <audio controls autoPlay={isPlaying} style={{ width: '100%' }}>
-		  //                                     <source src="/music/song1.mp3" type="audio/mpeg" />
-		  //                                             Your browser does not support the audio element.
-		  //                                                   </audio>
-		  //                                                         <button onClick={handlePrevTrack}>Previous</button>
-		  //                                                               <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
-		  //                                                                     <button onClick={handleNextTrack}>Next</button>
-		  //                                                                         </div>
-		  //                                                                           );
-		  //                                                                           }
-		  //
-		  //                                                                           export default MusicPlayer;
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const currentSong = songs[currentSongIndex];
+
+  const playPauseToggle = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const nextSong = () => {
+    if (currentSongIndex < songs.length - 1) {
+      setCurrentSongIndex(currentSongIndex + 1);
+    } else {
+      setCurrentSongIndex(0);
+    }
+  };
+
+  const previousSong = () => {
+    if (currentSongIndex > 0) {
+      setCurrentSongIndex(currentSongIndex - 1);
+    } else {
+      setCurrentSongIndex(songs.length - 1);
+    }
+  };
+
+  return (
+    <div className="music-player">
+      <h2>Now Playing: {currentSong.title}</h2>
+      <audio ref={audioRef} controls autoPlay={isPlaying} onEnded={nextSong}>
+        <source src={currentSong.source} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <div className="controls">
+        <button onClick={previousSong}>Previous</button>
+        <button onClick={playPauseToggle}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+        <button onClick={nextSong}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default MusicPlayer;
