ff89830c7b81aa44dc8b9e399b76da3f70f0d0c6
Juan Pablo Juliao <jpjuliao@gmail.com>
2023-10-14 04:22:05 -0500

    Update .eslintrc.js remove babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
-  parser: 'babel-eslint', // If you're using Babel
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   plugins: ['react'],
   rules: {
     'react/prop-types': 'off', // Disable prop-types validation
